387bb1422008def766cbac08f7b7363d59a05114
Miguel Angel <miguel.mzbi@gmail.com>
2016-02-08 21:21:53 -0600

    Inputs!

--- TC201/src/MANumber.java
+++ TC201/src/MANumber.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class MANumber{
 	private int x, xOriginal;
@@ -25,9 +26,15 @@ public class MANumber{
     }
 
     public static void main(String[] args) {
-    	MANumber n84 = new MANumber(285657);
-        int q = 192;
+    	Scanner reader = new Scanner(System.in);
+    	MANumber n84 = new MANumber(1423);
+    	int q;
+    	
+    	System.out.println("Enter a number: ");
+    	
+    	q = reader.nextInt();
         int r = n84.calcGCD(q);
+        
         System.out.println("gcd(" + n84.xOriginal + ", " + q + ") = " + r);
     }
 }
