8465a97ef425a08a89a460136e0df31209ddc8b7
Miguel Angel <miguel.mzbi@gmail.com>
2016-02-09 19:08:04 -0600

    Better variables

--- TC201/src/MANumber.java
+++ TC201/src/MANumber.java
@@ -1,41 +1,41 @@
 import java.util.Scanner;
 
 public class MANumber{
-	private int x, xOriginal;
+	private int num;
+	int numOriginal;
 	
-	public MANumber(int num){
-		this.xOriginal = num;
-		this.x = num;
+	public MANumber(int n){
+		this.numOriginal = n;
+		this.num = n;
 	}
 	
-	public int calcGCD(int y){
+	public int calcGCD(int nToCompare){
 		
-		if(this.x == 0){
-			return y;	
+		if(this.num == 0){
+			return nToCompare;	
 		}
 		
-		else if(y == 0){
-			return this.x;
+		else if(nToCompare == 0){
+			return this.num;
 		}
 		else{
-			int work = this.x % y;
-			this.x = y; 
-			return this.calcGCD(work);
+			int temporal = this.num % nToCompare;
+			this.num = nToCompare; 
+			return this.calcGCD(temporal);
 		}
         
     }
 
     public static void main(String[] args) {
-    	Scanner reader = new Scanner(System.in);
-    	MANumber n84 = new MANumber(1423);
-    	int q;
+    	Scanner scan = new Scanner(System.in);
+    	MANumber n84 = new MANumber(84);
     	
     	System.out.println("Enter a number: ");
     	
-    	q = reader.nextInt();
-        int r = n84.calcGCD(q);
+    	int comparison = scan.nextInt();
+        int result = n84.calcGCD(comparison);
         
-        System.out.println("gcd(" + n84.xOriginal + ", " + q + ") = " + r);
+        System.out.println("gcd(" + n84.numOriginal + ", " + comparison + ") = " + result);
     }
 }
 
