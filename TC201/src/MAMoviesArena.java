a8a6d6c0a0f09d5ed51803e1cf99cf4c96cb7fd1
miguel <osoazul97@gmail.com>
2016-02-18 13:59:31 -0600

    Update

--- TC201/src/MAMoviesArena.java
+++ TC201/src/MAMoviesArena.java
@@ -8,7 +8,7 @@ public class MAMoviesArena {
 	public static void main(String[] args) throws FileNotFoundException {
 		scan = new Scanner(System.in);
         //scan.useDelimiter("\\|\\^\\&");
-		MAMovies moviesData1 = new MAMovies("C:\\Users\\MiguelAngel\\git\\TC201\\TC201\\bin\\movies1.txt");
+		MAMovies moviesData1 = new MAMovies("/home/miguel/git/TC201/TC201/src/movies1.txt");
 		
 		moviesData1.readFile();
 		System.out.println("(a) Give me 2 movies separated by ( &, | , ^) to obatain the set of actors relationships\n(b) Give me name of an actor and find his/her co-actors\nWrite 'a' or 'b'.");
